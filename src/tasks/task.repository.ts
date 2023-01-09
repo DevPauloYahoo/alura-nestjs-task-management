a9b1b051bb6f0310eed8822de54cbc3a7b3a26fd
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-01-09 13:36:22 -0300

    Creating a Task Repository

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,5 @@
+import { Repository } from 'typeorm';
+
+import { TaskEntity } from './task.entity';
+
+export class TaskRepository extends Repository<TaskEntity> {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { TaskRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [TaskRepository],
   controllers: [TasksController],
   providers: [TasksService],
 })
