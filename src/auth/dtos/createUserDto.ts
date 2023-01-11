246347f3f19a8edca158b98b0be3917931acfb19
PgmAdmin <prmorais1302@gmail.com>
2023-01-11 12:51:45 -0300

    Validating password with expression language

--- src/auth/dtos/createUserDto.ts
+++ src/auth/dtos/createUserDto.ts
@@ -1,17 +1,22 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, Length, Matches } from 'class-validator';
+
+const REGEX = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
 
 export class CreateUserDto {
   @IsNotEmpty({
     message: 'Nome do usuário é obrigatório',
   })
-  @Length(4, 50, {
-    message: 'Nome do usuário deve ter entre 4 e 50 caracteres',
+  @Length(4, 30, {
+    message: 'Nome do usuário deve ter entre 4 e 30 caracteres',
   })
   username: string;
 
   @IsNotEmpty({ message: 'Senha é obrigatório' })
   @Length(4, 20, {
     message: 'Senha deve ter entre 4 e 20 caracteres',
+  })
+  @Matches(REGEX, {
+    message: 'A senha deve conter letra maiúscula e minúscula, número ou caracter especial ',
   })
   password: string;
 }
